3313be3e425e50d22c3b0c59f6521d740f183f43
Katerine <katerinehw@att.net>
2018-12-16 22:18:16 -0800

    updated code and inserted questions

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+module.exports = {
+    array: [
+        {
+            name: 'Flooty McPlasticnose',
+            photo: '/images/pic1.jpg',
+            scores: ['1', '1', '5', '1', '1', '1', '1', '1', '1', '1']
+        },
+        {
+            name: 'Twisty Sidewinder',
+            photo: '/images/pic2.jpg',
+            scores: ['1', '1', '5', '4', '1', '3', '1', '2', '1', '6']
+        },
+        {
+            name: 'Pepper VonFloofenstuffer',
+            photo: '/images/pick3.jpg',
+            scores: ['5', '6', '5', '1', '2', '1', '4', '6', '6', '2']
+        },
+        {
+            name: 'George McDreamy',
+            photo: '/images/pic4.jpg',
+            scores: ['5', '6', '5', '1', '2', '1', '4', '6', '6', '2']
+        }
+        {
+            name: 'Long Duck Dong',
+            photo: '/images/pic5.jpg',
+            scores: ['1', '3', '4', '4', '2', '1', '2', '6', '6', '3']
+        }
+    ]
+}
