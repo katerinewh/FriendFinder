81075e6b40414c53e937c1dc7a36edb00f9cfaaf
Katerine <katerinehw@att.net>
2018-12-18 09:54:55 -0800

    fixed linking issue with survey

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,27 +2,27 @@ module.exports = {
     array: [
         {
             name: 'Flooty McPlasticnose',
-            photo: 'images/pic1.jpg',
+            photo: '/pic1.jpg',
             scores: ['1', '1', '5', '1', '1', '1', '1', '1', '1', '1']
         },
         {
             name: 'Twisty Sidewinder',
-            photo: '/images/pic2.jpg',
+            photo: '/pic2.jpg',
             scores: ['1', '1', '5', '4', '1', '3', '1', '2', '1', '6']
         },
         {
             name: 'Pepper VonFloofenstuffer',
-            photo: '/images/pick3.jpg',
+            photo: '/pick3.jpg',
             scores: ['5', '6', '5', '1', '2', '1', '4', '6', '6', '2']
         },
         {
             name: 'George McDreamy',
-            photo: '/images/pic4.jpg',
+            photo: '/pic4.jpg',
             scores: ['5', '6', '5', '1', '2', '1', '4', '6', '6', '2']
         },
         {
             name: 'Long Duck Dong',
-            photo: '/images/pic5.jpg',
+            photo: '/pic5.jpg',
             scores: ['1', '3', '4', '4', '2', '1', '2', '6', '6', '3']
         }
     ]
